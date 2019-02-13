8f46585207e9b4939a76167cf69970f80d921cc4
Brian Hicks <brian@brianthicks.com>
2019-02-13 12:37:45 -0600

    pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,10 @@
-with import <nixpkgs> {};
+with import (builtins.fetchTarball rec {
+  # grab a hash from here: https://nixos.org/channels/
+  name = "nixpkgs-darwin-18.09pre153446.836d1d7c1cc";
+  url = "https://github.com/nixos/nixpkgs/archive/836d1d7c1cce26eda233dc925c8093e5a5d86ad3.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "1vibh9k2swmsq07v2fsqxhc0lxn0gnk48jsykxbb42bvlsxisxdi";
+}) {};
 
 stdenv.mkDerivation {
   name = "elm-edit";
